79e753a7c0061cbfbf3f2ae26f527b217a8761be
lucasconninck <lucasconninck72@gmail.com>
2025-06-10 20:11:46 -0300

    Add files via upload

---
+++ lucas/avl.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) {
+    ArvoreAVL arvore = new arvoreAVL();
+
+    int[] chaves = {10, 20, 30, 40, 50, 25}
+        for (int chave : chaves) {
+            arvore.raiz = avore.inserir(arvore.raiz, chave);
+        }
+        System.out.println("Percurso em ordem da arvore AVL")
+        arvore.percursoEmOrdem(arvore.raiz);
+}
